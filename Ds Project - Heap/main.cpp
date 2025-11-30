d0afc4ed0688237b67bc3df1e1a35039db3d3ac1
rawan-amin <rawan.skr.2005@gmail.com>
2025-11-30 23:43:54 +0200

    rawan

--- Ds Project - Heap/main.cpp
+++ Ds Project - Heap/main.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
@@ -7,6 +7,14 @@ int main(){
     cout << "amira";
     cout<<"jana";
     cout<<"all pulled?";
+<<<<<<< HEAD
+    cout << "sanya";
+=======
     cout<<"commit el awl ya jana";
+<<<<<<< HEAD
+>>>>>>> f2837134c1ed09b8d46bcc5ffba7d993ce3bbd5f
+=======
     cout << "yaraabbbbb";
+>>>>>>> 39be1d81dc4150ce42ff1dc7c85ab16e48649b0a
 }
+//hi
