d5d75b45457e6b770fbc9563130f98897c6919a4
janaakarara <126957375+janaakarara@users.noreply.github.com>
2025-12-03 15:43:40 +0200

    emptycppCHAIR

---
+++ Ds Project - Heap/Emptychairs.cpp
@@ -0,0 +1,15 @@
+#include "EmptyChair.hpp"
+using namespace std;
+EmptyChair::EmptyChair(int l, int r) : left(l), right(r) {}
+int EmptyChair::length() const { return right - left + 1; }
+int EmptyChair::distance() const { return (length() - 1) / 2; }
+int EmptyChair::middleseat(bool preference;) const{
+                                                    int len = length();
+                                                    if (len % 2 == 1) { 
+                                                    return left + len / 2;  
+                                                 }
+                                                 else { 
+                                                    return preference ? left + len / 2 - 1 : left + len / 2; 
+                                                }
+                                            }
+                    
