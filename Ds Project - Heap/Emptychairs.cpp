7e3a17b004f2c9c27c78788e7e47dd7c44bff176
Amirabotros <amirabotros198@gmail.com>
2025-12-03 16:36:57 +0200

    amira

--- Ds Project - Heap/Emptychairs.cpp
+++ Ds Project - Heap/Emptychairs.cpp
@@ -1,18 +1,35 @@
 #include "EmptyChair.hpp"
 using namespace std;
-EmptyChair::EmptyChair(int l, int r) : left(l), right(r) {}
-int EmptyChair::length() const { return right - left + 1; }
-int EmptyChair::distance() const { return (length() - 1) / 2; }
-int EmptyChair::middleseat(bool preference) const{
-                                                    int middle_index = (left + right)/2;
-                                                    int len = length();
-                                                    //if odd length
-                                                    if (len % 2 == 1) { 
-                                                    return middle_index + 1;  
-                                                 }
-                                                 else { 
-                                                    return preference ? middle_index / 2  : (middle_index / 2) +1 ; 
-                                                }
 
-                                            }
+EmptyChair::EmptyChair(int l, int r) : left(l), right(r) {
+    if (left > right) {
+        cerr << "Invalid segment: left > right";
+    }
+}
+
+ bool EmptyChair::isempty() const {
+    return length() == 0;
+}
+
+
+int EmptyChair::length() const { 
+    return right - left + 1;
+}
+
+int EmptyChair::distance() const { 
+    return (length() - 1) / 2;
+}
+
+int EmptyChair::middleseat(bool preference) const{
+    int middle_index = (left + right)/2;
+    int len = length();
+    //if odd length
+    if (len % 2 == 1) { 
+        return middle_index + 1;  
+    }
+    else { 
+        return preference ? middle_index / 2  : (middle_index / 2) +1 ; 
+    }
+
+}
                     
