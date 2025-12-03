1736c43ac70971178b741535159d5efc7db3af1f
janaakarara <126957375+janaakarara@users.noreply.github.com>
2025-12-03 16:15:18 +0200

    pref update in middleseat

--- Ds Project - Heap/Emptychairs.cpp
+++ Ds Project - Heap/Emptychairs.cpp
@@ -5,12 +5,13 @@ int EmptyChair::length() const { return right - left + 1; }
 int EmptyChair::distance() const { return (length() - 1) / 2; }
 int EmptyChair::middleseat(bool preference;) const{
                                                     int len = length();
+                                                    //if odd length
                                                     if (len % 2 == 1) { 
                                                     return left + len / 2;  
                                                  }
                                                  else { 
-                                                    return preference ? left + len / 2 - 1 : left + len / 2; 
+                                                    return preference ? len / 2  : (len / 2) +1 ; 
                                                 }
-                                                
+
                                             }
                     
