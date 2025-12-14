35ead4586531d124cfd2ac2b36ca353fb388bfdd
Amirabotros <amirabotros198@gmail.com>
2025-12-14 18:53:36 +0200

    amira

---
+++ Ds Project - Heap/BestseatComparator.hpp
@@ -0,0 +1,11 @@
+#ifndef bestseatcomparator
+#define bestseatcomparator
+#define Emptychairs
+
+#include <iostream>
+using namsepace std;
+
+
+class bestseatcomparator{
+     bool operator>(const Emptychairs& seg1, const Emptychairs& seg2);
+}
